354ca7c13e05ff2478e3c21dcda4371a89ccc0fb
effiecancode <faitheffie25@gmail.com>
2023-10-27 16:12:22 +0300

    cart urls fix

--- market/cart/urls.py
+++ market/cart/urls.py
@@ -6,6 +6,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path("addtocart/<int:product_id>/", views.add_to_cart, name='addtocart'),
-    path("removefromcart/int:cart_item_id/", views.remove_from_cart, name='removefromcart'),
+    path("removefromcart/<int:cart_item_id>/", views.remove_from_cart, name='removefromcart'),
     path("cart_detail", views.cart_detail, name='cart_detail')
 ]
