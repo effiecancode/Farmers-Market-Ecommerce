61f0d979b2354892b3a9ab0cf842a8b3a1c9bf91
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-04 00:41:42 +0300

    bootstrap5 on crispy forms

--- market/market/settings.py
+++ market/market/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
