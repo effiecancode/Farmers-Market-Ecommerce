32d358ff7bbd3f2e30d414823b66df611fbbfb01
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-03 12:44:37 +0300

    profile do away

--- market/Account/forms.py
+++ market/Account/forms.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
-
-from .models import Profile
 
 
 class RegistrationForm(UserCreationForm):
@@ -12,13 +9,3 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ["image", "bio"]
-
-class UpdateProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ["image", "bio"]
